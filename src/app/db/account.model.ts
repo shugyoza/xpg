4f7476588bfa00b8d15bb2b1354b3b61319b7eab
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-11-23 18:13:19 -0800

    add account model methods

---
+++ src/app/db/account.model.ts
@@ -0,0 +1,57 @@
+import { AccountDTO } from '../controllers/account/account.interface';
+import { db } from '../../main';
+import { constants } from '../shared/constants/account.constant';
+
+const { tableName } = constants;
+
+const findAll = async (): Promise<AccountDTO[]> => {
+  const text = 'SELECT * FROM "' + tableName + '";';
+
+  return await db
+    .any(text)
+    .then((result: any) => {
+      console.log(text, result);
+
+      return result;
+    })
+    .catch((error: Error) => console.error(error));
+};
+
+const findByEmail = async (
+  email: string,
+  tableName = constants.tableName
+): Promise<AccountDTO> => {
+  const text = 'SELECT * FROM "' + tableName + '"WHERE EMAIL = $1;';
+  const values = [email];
+
+  return await db
+    .one(text, values)
+    .then((result: any) => {
+      console.log(text, result);
+      return result;
+    })
+    .catch((error: Error) => console.error(error));
+};
+
+const findByUsername = async (
+  username: string,
+  tableName = constants.tableName
+): Promise<AccountDTO> => {
+  const text = 'SELECT * FROM "' + tableName + '" WHERE USERNAME = $1;';
+  const values = [username];
+  console.log({ text, values });
+
+  return await db
+    .one(text, values)
+    .then((result: any) => {
+      console.log(text, result);
+      return result;
+    })
+    .catch((error: Error) => console.error(error));
+};
+
+export default {
+  findAll,
+  findByEmail,
+  findByUsername,
+};

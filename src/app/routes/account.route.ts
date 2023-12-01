bebd91b53aeaf3b48735e2c370fe91ff1af2805b
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-12-01 12:23:16 -0800

    add validators to /register and /login routes

--- src/app/routes/account.route.ts
+++ src/app/routes/account.route.ts
@@ -7,15 +7,21 @@ import {
   login,
   logout,
 } from '../controllers/account/account.controller';
+import { accountValidators, loginValidators } from '../controllers/validators';
 
 export const account = express.Router();
 
 account.get('/', findAll);
+account.post('/register', accountValidators, register);
 
 const options = {
-  successMessage: 'login success',
-  failureMessage: 'login fail',
+  successMessage: 'success',
+  failureMessage: 'fail',
 };
-account.post('/login', passport.authenticate('local', options), login);
-account.post('/register', register);
+account.post(
+  '/login',
+  loginValidators,
+  passport.authenticate('local', options),
+  login
+);
 account.post('/logout', logout);
